045e5a63fee0d71c932bedbd150889bb131b4bed
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-25 17:57:13 -0600

    Feat & test: create and test subscription validations Ensure the Subscription model validates presence of all attributes and numericality of most of them to be saved as nums

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,5 @@
 class Subscription < ApplicationRecord
   belongs_to :user
+  validates_presence_of :title, :price, :status, :frequency, :tea_name, :user_id
+  validates_numericality_of :price, :status, :frequency
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it {should belong_to(:user)}
+  end
+
+  describe "validations" do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:price)}
+    it {should validate_numericality_of(:price)}
+    it {should validate_presence_of(:status)}
+    it {should validate_numericality_of(:status)}
+    it {should validate_presence_of(:frequency)}
+    it {should validate_numericality_of(:frequency)}
+    it {should validate_presence_of(:tea_name)}
+    it {should validate_presence_of(:user_id)}
+  end
 end

c2599489618f02fc34f4e186bc69f11058415c05
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-25 15:10:54 -0600

    Update spec_helper, rails_helper, and .gitignore Spec_helper, rails_helper, and .gitignore were updated to work with new Gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "simplecov"
+SimpleCov.start
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -61,4 +64,27 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.around do |example|
+    if example.metadata.key?(:vcr)
+      example.run
+    else
+      VCR.turned_off { example.run }
+    end
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  # config.filter_sensitive_data("Sike you thought") { ENV['GOOGLE_CLIENT_ID'] }
+  # config.filter_sensitive_data("Sike you thought") { ENV['GOOGLE_CLIENT_SECRET'] }
+  config.allow_http_connections_when_no_cassette = true
 end

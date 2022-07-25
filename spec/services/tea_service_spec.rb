ff5c2dd84326e5b17eaaa5e41a4ca23a95277b58
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-25 15:56:50 -0600

    Test: update tea_service test to test for data types

--- spec/services/tea_service_spec.rb
+++ spec/services/tea_service_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe TeaService do
     it "get_all_teas", :vcr do
       tea_data = TeaService.get_all_teas
 
-      expect(tea_data).to be_a(Hash)
-      binding.pry
+      expect(tea_data).to be_an(Array)
+      expect(tea_data[0][:_id]).to be_a(String)
+      expect(tea_data[0][:name]).to be_a(String)
+      expect(tea_data[0][:description]).to be_a(String)
+      expect(tea_data[0][:temperature]).to be_an(Integer)
+      expect(tea_data[0][:brew_time]).to be_an(Integer)
     end
   end
 end

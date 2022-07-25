af7c9921402bd701c8d7b2d91ac4c6c8664135d9
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-25 16:53:27 -0600

    Test: TeaService.get_tea_by_title() Write RSpec test to ensure the TeaService method get_tea_by_title(title) hits the API and returns a single tea's data based on the title (or name) passed in

--- spec/services/tea_service_spec.rb
+++ spec/services/tea_service_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe TeaService do
       expect(tea_data[0][:temperature]).to be_an(Integer)
       expect(tea_data[0][:brew_time]).to be_an(Integer)
     end
+
+    it "get_tea_by_title()", :vcr do
+      tea_data = TeaService.get_tea_by_title("green")
+
+      expect(tea_data).to be_a(Hash)
+      expect(tea_data[:_id]).to eq("5fa3fd48d5ba620017ec1c09")
+      expect(tea_data[:name]).to eq("green")
+      expect(tea_data[:description]).to eq("Rich in antioxidants and reduces inflammation.")
+      expect(tea_data[:temperature]).to eq(80)
+      expect(tea_data[:brew_time]).to eq(2)
+    end
   end
 end

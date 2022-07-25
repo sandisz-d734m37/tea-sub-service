9fe26c5d2a9275c6f2a9f1dc474ebef199c171fa
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-25 17:07:58 -0600

    Make create_teas class method the TeaFacade.create_teas class method is used to create Tea POROs after the facade gets a response from the TeaService. This was made because what that metod does will be repeated a few times within methods in the class.

--- app/facades/tea_facade.rb
+++ app/facades/tea_facade.rb
@@ -3,16 +3,20 @@ class TeaFacade
     def get_all_teas
       tea_data = TeaService.get_all_teas
       tea_data.map do |data|
-        Tea.new(
-          attributes: {
-            id: data[:_id],
-            title: data[:name],
-            description: data[:description],
-            temperature: data[:temperature],
-            brew_time: data[:brew_time]
-          }
-        )
+        create_teas(data)
       end
+    end
+
+    def create_teas(data)
+      Tea.new(
+        attributes: {
+          id: data[:_id],
+          title: data[:name],
+          description: data[:description],
+          temperature: data[:temperature],
+          brew_time: data[:brew_time]
+        }
+      )
     end
   end
 end

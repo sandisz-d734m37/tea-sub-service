3663082d058c9071879d3a60ac1ddce0925cf1bb
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-25 18:07:37 -0600

    Feat & test: Subscr.s have readable attrs Test and ensure a Subscription can be created and it's attributes an be read Also ensure Subscription enumerations work properly

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,16 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   validates_presence_of :title, :price, :status, :frequency, :tea_name, :user_id
-  validates_numericality_of :price, :status, :frequency
+  validates_numericality_of :price
+
+  enum status: {
+    "inactive" => 0,
+    "active" => 1
+  }
+
+  enum frequency: {
+    "weekly" => 0,
+    "bi-weekly" => 1,
+    "monthly" => 2
+  }
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -10,10 +10,36 @@ RSpec.describe Subscription, type: :model do
     it {should validate_presence_of(:price)}
     it {should validate_numericality_of(:price)}
     it {should validate_presence_of(:status)}
-    it {should validate_numericality_of(:status)}
     it {should validate_presence_of(:frequency)}
-    it {should validate_numericality_of(:frequency)}
     it {should validate_presence_of(:tea_name)}
     it {should validate_presence_of(:user_id)}
+  end
+
+  describe "it exists" do
+    before do
+      @user_1 = User.create!(
+        first_name: "User One",
+        last_name: "Test",
+        shipping_address: "123 Test street, Nowhere, NO, 12345, USA",
+        email: "user_one@test.com"
+      )
+      @sub_1 = @user_1.subscriptions.create!(
+        title: "Bi-Weekly Chamomile",
+        price: 20.0,
+        status: 1,
+        frequency: 1,
+        tea_name: "chamomile"
+      )
+    end
+
+    it "has readable attributes" do
+      expect(@sub_1).to be_a(Subscription)
+      expect(@sub_1.user).to eq(@user_1)
+      expect(@sub_1.title).to eq("Bi-Weekly Chamomile")
+      expect(@sub_1.price).to eq(20.0)
+      expect(@sub_1.status).to eq("active")
+      expect(@sub_1.frequency).to eq("bi-weekly")
+      expect(@sub_1.tea_name).to eq("chamomile")
+    end
   end
 end

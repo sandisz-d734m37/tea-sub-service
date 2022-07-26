df62c3c89f8198baa952d6dd16a952d09de61e6c
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-26 13:02:57 -0600

    Feat & test: Get all users endpoint Create the needed controller actions to get all users Write an RSpec request spec to ensure this endpoint runs.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,11 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def index
+    users = User.all
+    render json: UserSerializer.new(users)
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :shipping_address)
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -30,4 +30,44 @@ RSpec.describe "User endpoints" do
       expect(parsed_response[:data][:attributes][:shipping_address]).to eq(test_user.shipping_address)
     end
   end
+
+  describe "get requests" do
+    before do
+      @user_1 = User.create!(
+        first_name: "User 1",
+        last_name: "test",
+        email: "user1@test.com",
+        shipping_address: "123 Test st., Nowhere, NO 12321"
+      )
+      @user_2 = User.create!(
+        first_name: "User 2",
+        last_name: "test",
+        email: "user2@test.com",
+        shipping_address: "321 Fake ave., Somehere, SO 32123"
+      )
+    end
+    
+    it "can get all users" do
+      headers = {"CONTENT_TYPE" => "application/json"}
+      get "/api/v1/users", headers: headers
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      parsed_data = parsed_response[:data]
+      expect(parsed_data).to be_an(Array)
+      expect(parsed_data.length).to eq(2)
+      expect(parsed_data[0][:type]).to eq("user")
+      expect(parsed_data[1][:type]).to eq("user")
+      expect(parsed_data[0][:attributes][:first_name]).to eq(@user_1.first_name)
+      expect(parsed_data[1][:attributes][:first_name]).to eq(@user_2.first_name)
+      expect(parsed_data[0][:attributes][:last_name]).to eq(@user_1.last_name)
+      expect(parsed_data[1][:attributes][:last_name]).to eq(@user_2.last_name)
+      expect(parsed_data[0][:attributes][:email]).to eq(@user_1.email)
+      expect(parsed_data[1][:attributes][:email]).to eq(@user_2.email)
+      expect(parsed_data[0][:attributes][:shipping_address]).to eq(@user_1.shipping_address)
+      expect(parsed_data[1][:attributes][:shipping_address]).to eq(@user_2.shipping_address)
+    end
+  end
 end

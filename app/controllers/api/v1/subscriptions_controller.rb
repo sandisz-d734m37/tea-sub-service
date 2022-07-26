a68039175d79cea0253d5b0455534d8e3a8b82a5
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-26 13:54:00 -0600

    Feat & test: POST subscription endpoint Set up the controlelr and serializer to allow the client to send a POST request and create a subscription

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    user = User.find(params[:user_id])
+    subscription = user.subscriptions.create!(subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: :created
+    end
+  end
+
+  private
+  def subscription_params
+    params.require(:subscription).permit(:title, :price, :status, :frequency, :tea_name)
+  end
+end
---
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe "Subscriptions", type: :request do
+  describe "Create subscription" do
+    before do
+      @user_1 = User.create!(
+        first_name: "User 1",
+        last_name: "test",
+        email: "user1@test.com",
+        shipping_address: "123 Test st., Nowhere, NO 12321"
+      )
+      @user_2 = User.create!(
+        first_name: "User 2",
+        last_name: "test",
+        email: "user2@test.com",
+        shipping_address: "321 Fake ave., Somehere, SO 32123"
+      )
+    end
+
+    it "can create a subscription" do
+      sub_params = {
+        title: "User 2 Test Sub",
+        price: 20.0,
+        status: 1,
+        frequency: 0,
+        tea_name: "black"
+      }
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/subscriptions/#{@user_2.id}", headers: headers, params: JSON.generate(subscription: sub_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      parsed_data = parsed_response[:data]
+
+      expect(parsed_data).to be_a(Hash)
+      expect(parsed_data[:type]).to eq("subscription")
+      expect(parsed_data[:attributes][:title]).to eq("User 2 Test Sub")
+      expect(parsed_data[:attributes][:price]).to eq(20.0)
+      expect(parsed_data[:attributes][:status]).to eq("active")
+      expect(parsed_data[:attributes][:frequency]).to eq("weekly")
+      expect(parsed_data[:attributes][:tea_name]).to eq("black")
+
+      expect(parsed_data[:attributes]).to have_key(:user)
+      expect(parsed_data[:attributes][:user][:id]).to eq(@user_2.id)
+      expect(parsed_data[:attributes][:user][:first_name]).to eq(@user_2.first_name)
+      expect(parsed_data[:attributes][:user][:last_name]).to eq(@user_2.last_name)
+      expect(parsed_data[:attributes][:user][:email]).to eq(@user_2.email)
+      expect(parsed_data[:attributes][:user][:shipping_address]).to eq(@user_2.shipping_address)
+    end
+  end
+end

96a90fc5279473a30ca2ddb6dd86a81263d81387
Sandisz Thieme <sandiszthieme@gmail.com>
2022-07-26 15:41:27 -0600

    Feat & test: Update subscription This endpoint allows a user to update their subscription. The test specifically looks at the endpoints ability to update the status between 'active' and 'inactive'

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -7,6 +7,14 @@ class Api::V1::SubscriptionsController < ApplicationController
     end
   end
 
+  def update
+    subscription = Subscription.find(params[:id])
+    subscription.update(subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: :created
+    end
+  end
+
   private
   def subscription_params
     params.require(:subscription).permit(:title, :price, :status, :frequency, :tea_name)
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Subscriptions", type: :request do
-  describe "Create subscription" do
+  describe "Create/update subscription" do
     before do
       @user_1 = User.create!(
         first_name: "User 1",
@@ -49,6 +49,39 @@ RSpec.describe "Subscriptions", type: :request do
       expect(parsed_data[:attributes][:user][:last_name]).to eq(@user_2.last_name)
       expect(parsed_data[:attributes][:user][:email]).to eq(@user_2.email)
       expect(parsed_data[:attributes][:user][:shipping_address]).to eq(@user_2.shipping_address)
+    end
+
+    it "can update activate & deactivate a subscription (toggle status)" do
+      u1_sub1 = @user_1.subscriptions.create!(
+        title: "User 1 Test Subscription 1",
+        price: 10.0,
+        status: 1,
+        frequency: 1,
+        tea_name: "green"
+      )
+
+      expect(u1_sub1.status).to eq("active")
+
+      sub_params = {
+        status: 0
+      }
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+      patch "/api/v1/subscriptions/#{u1_sub1.id}", headers: headers, params: JSON.generate(subscription: sub_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      parsed_data = parsed_response[:data]
+      updated_sub = Subscription.find(u1_sub1.id)
+
+      expect(updated_sub.id).to eq(u1_sub1.id)
+      expect(updated_sub.status).to eq("inactive")
+      expect(parsed_data[:id].to_i).to eq(u1_sub1.id)
+      expect(parsed_data[:type]).to eq("subscription")
+      expect(parsed_data[:attributes][:status]).to eq("inactive")
+      expect(parsed_data[:attributes][:user][:id]).to eq(@user_1.id)
     end
   end
 end
